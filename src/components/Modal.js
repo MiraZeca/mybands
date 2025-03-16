d82865d5de295129e3842c6d2fc4938d9900cc3e
MiraZeca <84153544+MiraZeca@users.noreply.github.com>
2025-03-16 13:54:37 +0100

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -29,7 +29,6 @@ const Modal = ({currentBand}) => {
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
