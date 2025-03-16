84b015cd682c70f04ce59f5816597e00642d0128
MiraZeca <84153544+MiraZeca@users.noreply.github.com>
2025-03-17 00:03:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,35 +2,40 @@ import React from "react";
 import BandsList from "./components/BandsList";
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
-
+import { Container, Row, Col } from "reactstrap";  // Dodajemo Bootstrap Grid komponente
 
 class App extends React.Component {
     state = {
-        bands : [],
-        currentBand : {}
-    }
+        bands: [],
+        currentBand: {}
+    };
 
     changeCurrentBand = (band) => {
-        this.setState({currentBand:band})
+        this.setState({ currentBand: band });
+    };
+
+    componentDidMount() {
+        fetch('https://raw.githubusercontent.com/MiraZeca/mybands/master/bands_with_id.json')
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ bands: data });
+            });
     }
 
-    componentDidMount(){
-        fetch('https://raw.githubusercontent.com/MiraZeca/mybands/master/bands_with_id.json')
-        .then(res=>{
-            return res.json();
-        })
-        .then(data=>{
-            this.setState({bands:data})
-        })
-    }
-    render(){
+    render() {
         return (
             <>
-            <Navbar />
-            <BandsList bands={this.state.bands} changeCurrentBand={this.changeCurrentBand}/>
-            <Modal currentBand={this.state.currentBand}/>
+                <Navbar />
+                <Container>
+                    <Row>
+                        <Col xs="12" sm="6" md="4" lg="3" className="my-3">
+                            <BandsList bands={this.state.bands} changeCurrentBand={this.changeCurrentBand} />
+                        </Col>
+                    </Row>
+                </Container>
+                <Modal currentBand={this.state.currentBand} />
             </>
-        )
+        );
     }
 }
 
