b04696273726374d4834a58134e941e8fbb47c95
MiraZeca <84153544+MiraZeca@users.noreply.github.com>
2025-03-17 13:49:35 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-light bg-light">
             <a href="/" className="navbar-brand">React Bands App</a>
-            <a href="https://mirazeca.com/" className="navbar-brand">Back to CV</a>
+            <a href="https://mirazeca.com/">Back to CV</a>
         </nav>
     )
 }
